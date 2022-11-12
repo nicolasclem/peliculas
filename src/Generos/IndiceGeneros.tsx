ad7b81e36bcbd3047c78d7d79166ccd9e0e1e33a
Nicolas Clementi <clementinicolas@gmail.com>
2022-11-12 20:59:05 -0300

    Axios  env

--- src/Generos/IndiceGeneros.tsx
+++ src/Generos/IndiceGeneros.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { generoDTO } from './generosModel';
+import { pruebaAPI, urlGeneros } from '../Utils/endpoints';
 
 const IndiceGeneros = () => {
+  useEffect(()=>{
+    console.log(urlGeneros);
+    console.log(pruebaAPI);
+    
+    axios.get(urlGeneros)
+    .then((respuesta: AxiosResponse<generoDTO>)=>{
+      console.log(respuesta.data);
+      
+    })
+  },[])
   return (
       <> 
       <h3>Indice Generos</h3>
---
+++ src/Utils/endpoints.ts
@@ -0,0 +1,4 @@
+
+const apiURL= process.env.REACT_APP_API_URL// me tira undefined   resolver!!!!
+export  const pruebaAPI= `${apiURL}`  
+export const urlGeneros= `https://localhost:7265/api/generos`;
