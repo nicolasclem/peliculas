1c6af13a36bd49f731eb3891730efb477c16677b
Nicolas Clementi <clementinicolas@gmail.com>
2022-11-14 12:45:33 -0300

    table generos

--- src/Generos/IndiceGeneros.tsx
+++ src/Generos/IndiceGeneros.tsx
@@ -1,24 +1,53 @@
 
 import { Link } from 'react-router-dom'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import { generoDTO } from './generosModel';
 import {  urlGeneros } from '../Utils/endpoints';
+import ListadoGenerico from '../Utils/ListadoGenerico';
+import Button from '../Utils/Button';
 
 const IndiceGeneros = () => {
+  const [generos,setGeneros]= useState<generoDTO[]>();
   useEffect(()=>{
   
     axios.get(urlGeneros)
-    .then((respuesta: AxiosResponse<generoDTO>)=>{
-      console.log(respuesta.data);
-      
+    .then((respuesta: AxiosResponse<generoDTO[]>)=>{
+      setGeneros(respuesta.data)
     })
   },[])
   return (
       <> 
       <h3>Generos</h3>
       <Link className='btn btn-primary' to="crear">Crear Genero</Link>
-      <Link to="editar">Editar Genero</Link>
+
+      <ListadoGenerico listado={generos}>
+        <table className='table table-striped'>
+          <thead>
+              
+                <tr>
+                  <th>Accion</th>
+                  <th>Nombre</th>
+                </tr>
+              
+          </thead>
+          <tbody>
+              {generos?.map(genero=>
+                <tr key={genero.id}>
+                  <td>
+                    <Link className='btn btn-success' to={`/generos/${genero.id}`}>Editar</Link>
+                    <Button className='btn btn-danger'>Borrar</Button>
+                  </td>
+                  <td>
+                    {genero.nombre}
+                  </td>
+                </tr>)}
+          </tbody>
+        </table>
+
+
+
+      </ListadoGenerico>
       </>
       )
 }
