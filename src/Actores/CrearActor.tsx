5336eec0f431c38f63437661d0279a0689914008
Nicolas Clementi <clementinicolas@gmail.com>
2022-11-15 18:12:13 -0300

    crear Actor

--- src/Actores/CrearActor.tsx
+++ src/Actores/CrearActor.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import FormularioActores from './FormularioActores'
+import axios from 'axios';
+import { actorCreacionDTO } from './actores.model';
+import FormularioActores from './FormularioActores';
+import { urlActores } from '../Utils/endpoints';
+import { useNavigate } from 'react-router-dom';
+import { convertirActorAFormData } from '../Utils/formDataUtils';
 
 const CrearActor = () => {
+  const navigate= useNavigate()
+  async function crear( actor: actorCreacionDTO){
+    try {
+      const formData= convertirActorAFormData(actor);
+      await axios(
+        {
+          method:'post',
+          url:urlActores,
+          data:formData,
+          headers:{'Content-Type':'multipart/form-data'}
+      
+        }
+      )
+      navigate('/actores')
+   } catch (error) {
+     console.error(error);
+     
+ }
+}
   return (
     <>
     <h3>Crear Actor</h3>
 
     <FormularioActores 
     modelo={{nombre:'', fechaNacimiento: undefined}}
-    onSubmit={valores=> console.log(valores)
+    onSubmit={async valores=> await crear(valores)
+      
+      
     }
     />
     </>
--- src/Utils/endpoints.ts
+++ src/Utils/endpoints.ts
@@ -2,3 +2,4 @@
 const apiURL= process.env.REACT_APP_API_URL// me tira undefined   resolver!!!!=>>  solucionado solo reiniciar VS
 
 export const urlGeneros= `${apiURL}/generos`;
+export const urlActores=`${apiURL}/actores`;
